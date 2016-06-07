877df99f94201fe74377e97d04e0edbf1ad82856
talhakosen <talhakosen@hotmail.com>
2016-06-07 20:05:26 +0300

    QueryBuilder, UriMatcher implemented

--- app/src/main/java/tkosen/com/android_sql_sample/data/ContryProvider.java
+++ app/src/main/java/tkosen/com/android_sql_sample/data/ContryProvider.java
@@ -2,7 +2,9 @@ package tkosen.com.android_sql_sample.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -10,8 +12,31 @@ import android.support.annotation.Nullable;
  * Created by tctkosen on 07/06/16.
  */
 public class ContryProvider extends ContentProvider {
+    static final int COUNTRY = 100;
+    private static final SQLiteQueryBuilder sCountriesQueryBuilder;
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static {
+        sCountriesQueryBuilder = new SQLiteQueryBuilder();
+        sCountriesQueryBuilder.setTables(CountryContract.CountryEntry.TABLE_NAME);
+    }
+
+    private CountryDbHelper countryDbHelper;
+
+    static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = CountryContract.CountryEntry.CONTENT_AUTHORITY;
+        matcher.addURI(authority, CountryContract.CountryEntry.PATH_COUNTRY, COUNTRY);
+        return matcher;
+    }
+
+    public Cursor getCountries() {
+        return countryDbHelper.getReadableDatabase().query(CountryContract.CountryEntry.TABLE_NAME, null, "", null, "", "", "");
+    }
+
     @Override
     public boolean onCreate() {
+        countryDbHelper = new CountryDbHelper(getContext());
         return false;
     }
 
