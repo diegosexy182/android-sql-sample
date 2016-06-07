8e0b6c55fb6688c69b0f924e8f07e754b10a66e2
talhakosen <talhakosen@hotmail.com>
2016-06-07 12:47:09 +0300

    find elements

--- app/src/main/java/tkosen/com/android_sql_sample/MainActivity.java
+++ app/src/main/java/tkosen/com/android_sql_sample/MainActivity.java
@@ -2,12 +2,21 @@ package tkosen.com.android_sql_sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView list;
+    Button btn_add;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.list);
+        btn_add = (Button) findViewById(R.id.btn_add);
+
+
     }
 }
