80e04d106cc95491119c70488c243b8574e33467
talhakosen <talhakosen@hotmail.com>
2016-06-07 22:16:08 +0300

    loader_callback_implemented

--- app/src/main/java/tkosen/com/android_sql_sample/MainActivity.java
+++ app/src/main/java/tkosen/com/android_sql_sample/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +17,7 @@ import tkosen.com.android_sql_sample.data.CountryDbHelper;
 
 import static tkosen.com.android_sql_sample.data.CountryContract.CountryEntry;
 
-public class MainActivity extends AppCompatActivity implements FragmentDialog.OnFragmentInteractionListener {
+public class MainActivity extends AppCompatActivity implements FragmentDialog.OnFragmentInteractionListener, LoaderManager.LoaderCallbacks<Cursor> {
     ListView list;
     Button btn_add;
     CountryDbHelper mDbHelper;
@@ -66,5 +68,20 @@ public class MainActivity extends AppCompatActivity implements FragmentDialog.On
         countryCursor.close();
 
         return countryId;
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
     }
 }
